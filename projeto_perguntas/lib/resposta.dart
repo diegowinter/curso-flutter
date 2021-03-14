3e85730856363310e105797838c4400dff1eb460
Diego Winter <diego@diegowinter.dev>
2021-03-13 23:04:05 -0300

    project(perguntas): passando função callback

--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() onSelected;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.onSelected);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text(texto),
+        onPressed: onSelected,
+      ),
     );
   }
 }
