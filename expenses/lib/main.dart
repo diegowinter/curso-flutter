feed7e9747fe23adf556410f87ef0855eb6183ab
Diego Winter <diego@diegowinter.dev>
2021-03-17 16:07:04 -0300

    project(despesas pessoais): correção do formulário

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
     showModalBottomSheet(
       context: context,
       builder: (_) {
-        return TransactionForm(null);
+        return TransactionForm(_addTransaction);
       }
     );
   }
