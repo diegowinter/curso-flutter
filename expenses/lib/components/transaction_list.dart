4538d85f58739561a58f0f43303d187fa95bbd89
Diego Winter <diego@diegowinter.dev>
2021-03-17 23:32:29 -0300

    project(despesas pessoais): excluindo transação

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -5,7 +5,8 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  TransactionList(this.transactions);
+  final void Function(String) onRemove;
+  TransactionList(this.transactions, this.onRemove);
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +55,11 @@ class TransactionList extends StatelessWidget {
               subtitle: Text(
                 DateFormat('d/MMM/y').format(tr.date)
               ),
-              trailing: null,
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => onRemove(tr.id)
+              ),
             ),
           );
         },
