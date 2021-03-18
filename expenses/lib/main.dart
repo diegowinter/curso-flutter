5432740f83acd481525dbdc9b898a14f3c1a3374
Diego Winter <diego@diegowinter.dev>
2021-03-17 23:22:52 -0300

    project(despesas pessoais): transação com data

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -53,32 +53,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: 't0',
-      title: 'Conta antiga',
-      value: 400,
-      date: DateTime.now().subtract(Duration(days: 33))
-    ),
-    Transaction(
-      id: 't1',
-      title: 'Novo tênis de corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 3))
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 211.30,
-      date: DateTime.now().subtract(Duration(days: 4))
-    ),
-    Transaction(
-      id: 't3',
-      title: 'Videogame novo',
-      value: 2799.99,
-      date: DateTime.now()
-    )
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -97,12 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  _addTransaction(String title, double value) {
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now()
+      date: date
     );
 
     setState(() {
