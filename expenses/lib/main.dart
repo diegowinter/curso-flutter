a9b984628203d96e8eb67b51c87e022d6293b398
Diego Winter <diego@diegowinter.dev>
2021-03-19 16:03:56 -0300

    project(despesas pessoais): objeto MediaQuery

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -105,7 +105,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text(
@@ -114,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // para quando o usuário deseja ter fontes maiores, por questões de
         // acessibilidade.
         // style: TextStyle(
-        //   fontSize: 20 * MediaQuery.of(context).textScaleFactor
+        //   fontSize: 20 * mediaQuery.textScaleFactor
         // ),
       ),
       actions: [
@@ -134,9 +135,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final availableHeight = MediaQuery.of(context).size.height 
+    final availableHeight = mediaQuery.size.height 
       - appBar.preferredSize.height
-      - MediaQuery.of(context).padding.top;
+      - mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
