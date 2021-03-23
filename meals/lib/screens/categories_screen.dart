bd755b07ae8b620efe5ca8acd66560fa695bb9d5
Diego Winter <diego@diegowinter.dev>
2021-03-22 22:56:09 -0300

    project(meals): tela de categorias

--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-         maxCrossAxisExtent: 200,
-         childAspectRatio: 3 / 2,
-         crossAxisSpacing: 20,
-         mainAxisSpacing: 20
-       ),
-       children: [
-         
-       ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
