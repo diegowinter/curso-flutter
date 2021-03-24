04c5dcc416b824240cf48028309601e2a57c2147
Diego Winter <diego@diegowinter.dev>
2021-03-23 23:11:02 -0300

    project(meals): adicionando links no drawer

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
 import 'screens/tabs_screen.dart';
+import 'screens/settings_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -31,7 +31,8 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen()
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen()
       },
       // onGenerateRoute: (settings) {
       //   if (settings.name == '/alguma-coisa') {
