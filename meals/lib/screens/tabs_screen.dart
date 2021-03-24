9f660aacd440a8b12d53de5387f4024942b63814
Diego Winter <diego@diegowinter.dev>
2021-03-23 22:28:02 -0300

    project(meals): adicionar TabBar na parte inferior

--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -2,33 +2,58 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+  final List<Map<String, Object>> _screens = [
+    {
+      'title': 'Categorias',
+      'screen': CategoriesScreen()
+    },
+    {
+      'title': 'Favoritos',
+      'screen': FavoriteScreen()
+    },
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // quantidade de abas que estão sendo definidas abaixo
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.kitchen),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: 'Favoritos',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _screens[_selectedScreenIndex]['title']
+        ),
+      ),
+      body: _screens[_selectedScreenIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedScreenIndex, // importante para saber o index
+                                            // atual.
+        // type: BottomNavigationBarType.shifting, // para aquele tipo de barra
+                                                   // com animação.
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.kitchen),
+            label: 'Categorias'
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen()
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos'
+          ),
+        ],
       ),
     );
   }
