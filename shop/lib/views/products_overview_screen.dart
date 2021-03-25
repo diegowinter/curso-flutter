ae67a1e375dba30f116cc80ae264edac3968e79e
Diego Winter <diego@diegowinter.dev>
2021-03-24 21:50:15 -0300

    project(shop): usando provider

--- shop/lib/views/products_overview_screen.dart
+++ shop/lib/views/products_overview_screen.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
-import '../data/dummy_data.dart';
-import '../models/product.dart';
+import 'package:provider/provider.dart';
+
+//import '../models/product.dart';
 import '../widgets/produtct_item.dart';
+import '../providers/products.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
 
   @override
   Widget build(BuildContext context) {
+    // items é o getter que tem no provider.
+    //final List<Product> loadedProducts = Provider.of<Products>(context).items;
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
       body: GridView.builder(
         padding: const EdgeInsets.all(16),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
+        itemCount: products.length,
+        itemBuilder: (ctx, index) => ProductItem(products[index]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
