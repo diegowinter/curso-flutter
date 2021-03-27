3823092f71e321773f7e524e667a96bbdff0d4bc
Diego Winter <diego@diegowinter.dev>
2021-03-27 15:27:35 -0300

    project(shop): marcando produto como favorito

--- shop/lib/models/product.dart
+++ shop/lib/models/product.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class Product {
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -14,6 +14,11 @@ class Product {
     @required this.description,
     @required this.price,
     @required this.imageUrl,
-    isFavorite = false
+    this.isFavorite = false
   });
+
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
