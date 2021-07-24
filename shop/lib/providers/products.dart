9661b55d5a7f3a714bfbdf6f5500a355e536d51b
Diego Winter <diego@diegowinter.dev>
2021-07-24 00:05:41 -0300

    projetc(shop): excluir produto

--- shop/lib/providers/products.dart
+++ shop/lib/providers/products.dart
@@ -50,4 +50,12 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    final int index = _items.indexWhere((prod) => prod.id == id);
+    if (index >= 0) {
+      _items.removeWhere((prod) => prod.id == id);
+      notifyListeners();
+    }
+  }
 }
