0789fa22cceac30138eb7fd9eca72b8277b9d664
Diego Winter <diego@diegowinter.dev>
2021-07-27 21:58:27 -0300

    project(shop): carregando os pedidos

--- shop/lib/views/orders_screen.dart
+++ shop/lib/views/orders_screen.dart
@@ -4,7 +4,28 @@ import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/app_drawer.dart';
 import 'package:shop/widgets/order_widget.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<Orders>(context, listen: false).loadOrders().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  Future<void> _refreshOrders() async {
+    return Provider.of<Orders>(context, listen: false).loadOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Orders orders = Provider.of(context);
@@ -14,10 +35,17 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Meus pedidos'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, index) => OrderWidget(orders.items[index])
-      ),
+      body: _isLoading
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : RefreshIndicator(
+            onRefresh: () => _refreshOrders(),
+            child: ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, index) => OrderWidget(orders.items[index])
+            ),
+        ),
     );
   }
 }
