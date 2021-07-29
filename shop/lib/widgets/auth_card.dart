c7f60ef40d3150fa5a37eb53c130244fe559dc15
Diego Winter <diego@diegowinter.dev>
2021-07-29 00:29:43 -0300

    project(shop): tela de autenticação

---
+++ shop/lib/widgets/auth_card.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+enum AuthMode { Signup, Login }
+
+class AuthCard extends StatefulWidget {
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  GlobalKey<FormState> _form = GlobalKey();
+  bool _isLoading = false;
+  AuthMode _authMode = AuthMode.Login;
+  final _passwordController = TextEditingController();
+
+  Map<String, String> _authData = {
+    'email': '',
+    'password': ''
+  };
+
+  void _submit() {
+    if (!_form.currentState.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    _form.currentState.save();
+
+    if (_authMode == AuthMode.Login) {
+      // Login
+    } else {
+      // Registro
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _switchAuthMode() {
+    setState(() {
+      if (_authMode == AuthMode.Login) {
+        _authMode = AuthMode.Signup;
+      } else {
+        _authMode = AuthMode.Login;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return Card(
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Container(
+        height: _authMode == AuthMode.Login ? 290 : 372,
+        width: deviceSize.width * 0.75,
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty || !value.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) => _authData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                controller: _passwordController,
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty || value.length < 5) {
+                    return 'Informe uma senha válida';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) => _authData['password'] = value,
+              ),
+              if (_authMode == AuthMode.Signup)
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Confirmar senha'),
+                  obscureText: true,
+                  validator: _authMode == AuthMode.Signup ? (value) {
+                    if (value != _passwordController.text) {
+                      return 'As senhas são diferentes';
+                    }
+
+                    return null;
+                  } : null,
+                ),
+              Spacer(),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 8.0
+                    )
+                  ),   
+                  child: Text(
+                    _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR'
+                  ),
+                  onPressed: _submit,
+                ),
+              TextButton(
+                onPressed: _switchAuthMode,
+                child: Text(
+                  'ALTERNAR PARA ${_authMode == AuthMode.Login ? 'REGISTRAR' : 'LOGIN'}'
+                ),
+              )
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
