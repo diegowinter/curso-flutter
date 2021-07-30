2fbf1d88f73b210b1297d2f2be62a2eb8c3cbfec
Diego Winter <diego@diegowinter.dev>
2021-07-29 22:40:04 -0300

    project(shop): tratando erros de autenticação

--- shop/lib/providers/auth.dart
+++ shop/lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
 
 class Auth with ChangeNotifier {
   Future<void> _authenticate(String email, String password, String urlSegment) async {
@@ -14,8 +15,11 @@ class Auth with ChangeNotifier {
         'returnSecureToken': true
       })
     );
-
-    print(json.decode(response.body));
+    final responseBody = json.decode(response.body);
+    
+    if (responseBody['error'] != null) {
+      throw AuthException(responseBody['error']['message']);
+    }
 
     return Future.value();
   }
--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/exceptions/auth_exception.dart';
 import 'package:shop/providers/auth.dart';
 
 enum AuthMode { Signup, Login }
@@ -20,6 +21,24 @@ class _AuthCardState extends State<AuthCard> {
     'password': ''
   };
 
+  void _showErrorDialog(String msg) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Ocorreu um erro'),
+        content: Text(msg),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('FECHAR'),
+          )
+        ],
+      )
+    );
+  }
+
   Future<void> _submit() async {
     if (!_form.currentState.validate()) {
       return;
@@ -33,11 +52,18 @@ class _AuthCardState extends State<AuthCard> {
 
     Auth auth = Provider.of(context, listen: false);
 
-    if (_authMode == AuthMode.Login) {
-      await auth.login(_authData['email'], _authData['password']);
-    } else {
-      await auth.signup(_authData['email'], _authData['password']);
+    try {
+      if (_authMode == AuthMode.Login) {
+        await auth.login(_authData['email'], _authData['password']);
+      } else {
+        await auth.signup(_authData['email'], _authData['password']);
+      }
+    } on AuthException catch (error) {
+      _showErrorDialog(error.toString());
+    } catch (error) {
+      _showErrorDialog('Ocorreu um erro inesperado.');
     }
+    
 
     setState(() {
       _isLoading = false;
