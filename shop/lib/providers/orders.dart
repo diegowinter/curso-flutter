2715bd54eb0c429a9c73650e8f5da9bd6ccc849f
Diego Winter <diego@diegowinter.dev>
2021-07-30 00:27:28 -0300

    project(shop): associar pedido ao usuário

--- shop/lib/providers/orders.dart
+++ shop/lib/providers/orders.dart
@@ -25,9 +25,10 @@ class Order {
 class Orders with ChangeNotifier {
   String _baseUrl = '${Constants.BASE_API_URL}/orders';
   String _token;
+  String _userId;
   List<Order> _items = [];
 
-  Orders([this._token, this._items = const []]);
+  Orders([this._token, this._userId, this._items = const []]);
 
   List<Order> get items {
     return [..._items];
@@ -39,7 +40,7 @@ class Orders with ChangeNotifier {
 
   Future<void> loadOrders() async {
     List<Order> loadedItems = [];
-    final response = await http.get(Uri.parse('$_baseUrl.json?auth=$_token'));
+    final response = await http.get(Uri.parse('$_baseUrl/$_userId.json?auth=$_token'));
     Map<String, dynamic> data = json.decode(response.body);
 
     _items.clear();
@@ -71,7 +72,7 @@ class Orders with ChangeNotifier {
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('$_baseUrl.json?auth=$_token'),
+      Uri.parse('$_baseUrl/$_userId.json?auth=$_token'),
       body: json.encode({
         'total': cart.totalAmount,
         'date': date.toIso8601String(),
