e35285984c0f02ec2b859c2d14379ad151e55d09
Diego Winter <diego@diegowinter.dev>
2021-07-31 18:38:50 -0300

    project(shop): criando animação do zero

--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -10,11 +10,46 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin {
   GlobalKey<FormState> _form = GlobalKey();
   bool _isLoading = false;
   AuthMode _authMode = AuthMode.Login;
   final _passwordController = TextEditingController();
+
+  AnimationController _controller;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300
+      )
+    );
+
+    _heightAnimation = Tween(
+      begin: Size(double.infinity, 290),
+      end: Size(double.infinity, 371),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear
+      )
+    );
+
+    _heightAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   Map<String, String> _authData = {
     'email': '',
@@ -74,8 +109,10 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       if (_authMode == AuthMode.Login) {
         _authMode = AuthMode.Signup;
+        _controller.forward();
       } else {
         _authMode = AuthMode.Login;
+        _controller.reverse();
       }
     });
   }
@@ -90,7 +127,8 @@ class _AuthCardState extends State<AuthCard> {
         borderRadius: BorderRadius.circular(10.0)
       ),
       child: Container(
-        height: _authMode == AuthMode.Login ? 290 : 372,
+        //height: _authMode == AuthMode.Login ? 290 : 372,
+        height: _heightAnimation.value.height,
         width: deviceSize.width * 0.75,
         padding: const EdgeInsets.all(16.0),
         child: Form(
