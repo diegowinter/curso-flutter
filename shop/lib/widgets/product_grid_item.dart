cad2f0ff7ada6073d38a839ab3ab9d79d1c2e6ef
Diego Winter <diego@diegowinter.dev>
2021-07-31 20:34:39 -0300

    project(shop): animação no carregamento das imagens

--- shop/lib/widgets/product_grid_item.dart
+++ shop/lib/widgets/product_grid_item.dart
@@ -26,9 +26,12 @@ class ProductGridItem extends StatelessWidget {
               arguments: product
             );
           },
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(
+              product.imageUrl,
+            ),
+            fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
