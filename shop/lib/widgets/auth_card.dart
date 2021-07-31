403add439ecfc34083ded62926c4bf42cfad047c
Diego Winter <diego@diegowinter.dev>
2021-07-31 18:51:21 -0300

    project(shop): usando widget AnimatedContainer

--- shop/lib/widgets/auth_card.dart
+++ shop/lib/widgets/auth_card.dart
@@ -19,27 +19,31 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   AnimationController _controller;
   Animation<Size> _heightAnimation;
 
-  @override
-  void initState() {
-    super.initState();
+  // @override
+  // void initState() {
+  //   super.initState();
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 300
-      )
-    );
+  //   _controller = AnimationController(
+  //     vsync: this,
+  //     duration: Duration(
+  //       milliseconds: 300
+  //     )
+  //   );
 
-    _heightAnimation = Tween(
-      begin: Size(double.infinity, 290),
-      end: Size(double.infinity, 371),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.linear
-      )
-    );
-  }
+  //   _heightAnimation = Tween(
+  //     begin: Size(double.infinity, 290),
+  //     end: Size(double.infinity, 371),
+  //   ).animate(
+  //     CurvedAnimation(
+  //       parent: _controller,
+  //       curve: Curves.linear
+  //     )
+  //   );
+
+  //   _heightAnimation.addListener(() {
+  //     setState(() {});
+  //   });
+  // }
 
   @override
   void dispose() {
@@ -105,10 +109,10 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
     setState(() {
       if (_authMode == AuthMode.Login) {
         _authMode = AuthMode.Signup;
-        _controller.forward();
+        // _controller.forward();
       } else {
         _authMode = AuthMode.Login;
-        _controller.reverse();
+        // _controller.reverse();
       }
     });
   }
@@ -122,83 +126,81 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0)
       ),
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.decelerate,
+        height: _authMode == AuthMode.Login ? 290 : 372,
+        //height: _heightAnimation.value.height,
+        width: deviceSize.width * 0.75,
+        padding: const EdgeInsets.all(16.0),
         child: Form(
-            key: _form,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || !value.contains('@')) {
-                      return 'Informe um e-mail válido';
-                    }
+          key: _form,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.isEmpty || !value.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
 
-                    return null;
-                  },
-                  onSaved: (value) => _authData['email'] = value,
-                ),
+                  return null;
+                },
+                onSaved: (value) => _authData['email'] = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                controller: _passwordController,
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty || value.length < 5) {
+                    return 'Informe uma senha válida';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) => _authData['password'] = value,
+              ),
+              if (_authMode == AuthMode.Signup)
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Senha'),
-                  controller: _passwordController,
+                  decoration: InputDecoration(labelText: 'Confirmar senha'),
                   obscureText: true,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Informe uma senha válida';
+                  validator: _authMode == AuthMode.Signup ? (value) {
+                    if (value != _passwordController.text) {
+                      return 'As senhas são diferentes';
                     }
 
                     return null;
-                  },
-                  onSaved: (value) => _authData['password'] = value,
+                  } : null,
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Confirmar senha'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup ? (value) {
-                      if (value != _passwordController.text) {
-                        return 'As senhas são diferentes';
-                      }
-
-                      return null;
-                    } : null,
-                  ),
-                Spacer(),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30)
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 30.0,
-                        vertical: 8.0
-                      )
-                    ),   
-                    child: Text(
-                      _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR'
+              Spacer(),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)
                     ),
-                    onPressed: _submit,
-                  ),
-                TextButton(
-                  onPressed: _switchAuthMode,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 8.0
+                    )
+                  ),   
                   child: Text(
-                    'ALTERNAR PARA ${_authMode == AuthMode.Login ? 'REGISTRAR' : 'LOGIN'}'
+                    _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR'
                   ),
-                )
-              ],
-            ),
+                  onPressed: _submit,
+                ),
+              TextButton(
+                onPressed: _switchAuthMode,
+                child: Text(
+                  'ALTERNAR PARA ${_authMode == AuthMode.Login ? 'REGISTRAR' : 'LOGIN'}'
+                ),
+              )
+            ],
           ),
-        builder: (ctx, child) => Container(
-          //height: _authMode == AuthMode.Login ? 290 : 372,
-          height: _heightAnimation.value.height,
-          width: deviceSize.width * 0.75,
-          padding: const EdgeInsets.all(16.0),
-          child: child
         )
       )
     );
