41bf3192e3fd3feb10d7a299fddb011568931439
Diego Winter <diego@diegowinter.dev>
2021-07-31 21:41:43 -0300

    project(shop): animando a tela de pedidos

--- shop/lib/widgets/order_widget.dart
+++ shop/lib/widgets/order_widget.dart
@@ -16,57 +16,63 @@ class _OrderWidgetState extends State<OrderWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
-          ),
-          if(_expanded)
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 15,
-                vertical: 4
+    final itemsHeight = widget.order.products.length * 25.0 + 10;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? itemsHeight + 92 : 92,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)
               ),
-              height: widget.order.products.length * 23.0 + 10,
-              child: ListView(
-                children: widget.order.products.map((product) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.title,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        '${product.quantity}x R\$ ${product.price}',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey
-                        ),
-                      )
-                    ],
-                  );
-                }).toList()
+              trailing: IconButton(
+                icon: Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
               ),
-            )
-        ],
+            ),
+            if(_expanded)
+              AnimatedContainer(
+                duration: Duration(milliseconds: 400),
+                height: _expanded ? itemsHeight : 0,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 4
+                ),
+                child: ListView(
+                  children: widget.order.products.map((product) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          product.title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          '${product.quantity}x R\$ ${product.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey
+                          ),
+                        )
+                      ],
+                    );
+                  }).toList()
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
