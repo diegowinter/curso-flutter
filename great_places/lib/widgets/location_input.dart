dba5881d284e377f2e70bebf274aa69d761bc443
Diego Winter <diego@diegowinter.dev>
2021-08-14 15:46:52 -0300

    project(great places): obtendo localização atual

--- great_places/lib/widgets/location_input.dart
+++ great_places/lib/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +8,12 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +48,7 @@ class _LocationInputState extends State<LocationInput> {
                   Text('Localização atual')
                 ],
               ),
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
             ),
             TextButton(
               child: Row(
