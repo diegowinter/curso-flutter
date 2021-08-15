3bf3740d62bb7d316645105577b804b6a18e9ece
Diego Winter <diego@diegowinter.dev>
2021-08-15 20:09:39 -0300

    project(chat): evoluindo tela autenticação

--- chat/lib/widgets/auth_form.dart
+++ chat/lib/widgets/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:chat/models/auth_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -5,42 +6,93 @@ class AuthForm extends StatefulWidget {
   _AuthFormState createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final AuthData _authData = AuthData();
+
+  _submit() {
+    bool isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      print(_authData);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
         margin: EdgeInsets.all(20),
-        child: SingleChildScrollView(
+        child: AnimatedSize(
+          vsync: this,
+          duration: Duration(milliseconds: 150),
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome',
+                  if (_authData.isSignup)
+                    TextFormField(
+                      key: ValueKey('name'),
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                      ),
+                      onChanged: (value) => _authData.name = value,
+                      validator: (value) {
+                        if (value == null || value.trim().length < 4) {
+                          return 'Nome deve ter no mínimo 4 caracteres.';
+                        }
+                        return null;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('email'),
                     decoration: InputDecoration(
                       labelText: 'E-mail',
                     ),
+                    onChanged: (value) => _authData.email = value,
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return 'Forneça um e-mail válido.';
+                      }
+
+                      return null;
+                    },
                   ),
                   TextFormField(
+                    key: ValueKey('password'),
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Senha',
                     ),
+                    onChanged: (value) => _authData.password = value,
+                    validator: (value) {
+                      if (value == null || value.trim().length < 8) {
+                        return 'Senha deve ter no mínimo 6 caracteres.';
+                      }
+                      return null;
+                    },
                   ),
                   SizedBox(height: 12),
                   ElevatedButton(
-                    child: Text('Entrar'),
-                    onPressed: () {},
+                    child: Text(_authData.isLogin ? 'Entrar' : 'Cadastrar'),
+                    onPressed: _submit,
                   ),
                   TextButton(
-                    child: Text('Criar uma nova conta?'),
-                    onPressed: () {},
+                    child: Text(
+                      _authData.isLogin
+                          ? 'Criar uma nova conta?'
+                          : 'Já possui uma conta?',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _authData.toggleMode();
+                      });
+                    },
                   )
                 ],
               ),
