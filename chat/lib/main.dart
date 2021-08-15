6de614560f81ee405326a5f1e048fbee36717005
Diego Winter <diego@diegowinter.dev>
2021-08-15 18:32:04 -0300

    project(chat): integração Flutter/Firestore

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp().whenComplete(() => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
